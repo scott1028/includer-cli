fb06cf0b0af548ba0cffcaa344fa036ad44a43e4
scott.lan <scott.lan@taisys.com>
2017-06-03 16:25:35 +0800

    update code

--- README.md
+++ README.md
@@ -15,18 +15,24 @@
 
 # Usage
 
+- Included file name startsWith `_`.
+- Default prefix is `@@`.
+
 ```
-$ ./node_modules/.bin/includer -s src -d dist
+$ ./node_modules/.bin/includer -s=src -d=dist -p=#
+	( handler #include('./_lib.txt') )
 	... or
-$ ./node_modules/.bin/includer -s src -d dist -w
+$ ./node_modules/.bin/includer -s=src -d=dist
 	... or
-$ ./node_modules/.bin/includer -s src -d dist -t tpl
-	( skip file with filename contains ****.tpl.*** )
+$ ./node_modules/.bin/includer -s=src -d=dist -w
+	... or
+$ ./node_modules/.bin/includer -s=src -d=dist -t=tpl
+	( handle file with filename contains *.tpl.* or *.tpl )
 ```
 
 # Example
 
 ```
 # main.js
-@@include('data.tpl.txt')
+@@include('_data.tpl.txt')
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -22,10 +22,11 @@ let rootDir = `${cwd}/${src}`;
 let distDir = `${cwd}/${dist}`;
 
 // 
-let srcTpl = [`${rootDir}/**/*`,
-              `!${rootDir}/**/*.${ext}.*`,
-              `${rootDir}/**/.*`,
-              `!${rootDir}/**/.*.${ext}.*`,];
+let srcTpl = [`${rootDir}/**/*.${ext}`,
+              `${rootDir}/**/.*.${ext}`,
+              `${rootDir}/**/*.${ext}.*`,
+              `${rootDir}/**/.*.${ext}.*`,
+              `!${rootDir}/**/_*`];
 
 gulp.task('include', function(){
     console.log('=> source has changed!!');
@@ -41,6 +42,11 @@ gulp.task('include', function(){
             basepath: '@file'
         }))
         .pipe(rename(function(path){
+            console.log(path);
+            if(path.basename.endsWith(`.${ext}`))
+                return path.basename = path.basename.slice(0, path.basename.length - `.${ext}`.length);
+            if(path.extname.endsWith(`.${ext}`))
+                return path.extname = path.extname.slice(0, path.extname.length - `.${ext}`.length);
             // TODO: Next version feature
             // console.log([path.basename]);
             // console.log([path.dirname]);
